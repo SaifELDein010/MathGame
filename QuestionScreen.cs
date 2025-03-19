d19ea835da43b9bf8623128c58cd7799014d689c
saif el_dein tarek <saifel.dein01010@gmail.com>
2025-03-19 16:24:08 +0200

    Add 2 methods for design question design

--- QuestionScreen.cs
+++ QuestionScreen.cs
@@ -4,6 +4,32 @@ namespace MathGame
     class QuestionScreen
     {
 
+        static private void HeaderSreen()
+        {
+
+            Console.Write("\n\t\t\t\t\tM a t h  -  game  -  Q u e s t i o n\n\n");
+
+        }
+
+        static private void QuestionDesign()
+        {
+
+            Console.WriteLine("\t\t\t\t\t    _-_-_-_-_-_-_-_-_-_-_-_-_-_\n");
+
+            //Console.WriteLine($"\t\t\t\t\t Question [2]:");
+
+            //Console.WriteLine($"\t\t\t\t\t Number 1");
+            //Console.Write($"\t\t\t\t\t Number 2");
+            //Console.WriteLine($"\t\t\t\t\t Operation");
+            //Console.WriteLine("\t\t\t\t\t ----------------");
+
+            //Get answer from user
+            //Send response correct or wronge with rigth answer
+
+            Console.WriteLine("\n\t\t\t\t\t    -_-_-_-_-_-_-_-_-_-_-_-_-_-");
+
+        }
+
 
 
     }
