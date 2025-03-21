a9482f5d99d9677b1e7727224b83b3c1f69befd8
saif el_dein tarek <saifel.dein01010@gmail.com>
2025-03-21 23:23:31 +0200

    Add method to get range of number based on level

--- GameQuestion.cs
+++ GameQuestion.cs
@@ -1,12 +1,23 @@
 ﻿
 
+using System.Security.Cryptography;
+
 namespace MathGame
 {
     class GameQuestion
     {
 
+        static private int from = 0;
+        static private int to = 0;
 
 
+        static private void SetRangeOfNumber(int _from, int _to)
+        {
+
+            from = _from;
+            to = _to;
+
+        }
 
     }
 
